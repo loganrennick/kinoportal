607b0130c2a242c17b0b4e6dd8e6be4f88755cc0
loganrennick <loganrennick@gmail.com>
2018-08-16 22:18:47 -0600

    added favicon and improved search

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Redirect } from 'react-router';
 
 export default class Navbar extends Component {
     constructor (props) {
         super(props);
         this.state = {
-          query: ""
+          query: "",
+          redirect: false,
         };
         this._onSelect = this._onSelect.bind(this);
-        this.queryInput = this.queryInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     _onSelect () {
         this.setState({query: this.search.value});
     }
-    queryInput () {
-        this._onSelect();
-        console.log("query");
-        if(this.props.handleQueryChange){
-            console.log("input");
-            this.props.handleQueryChange(this.state.query);
+    handleSubmit(event) {
+        event.preventDefault();
+        if(this.props.clearQuery){
+            this._onSelect();
+            console.log("query");
+            if(this.props.handleQueryChange){
+                console.log("input");
+                this.props.handleQueryChange(this.state.query);
+            }
         }
-    }
-    handleKeyPress(event) {
-        if(event.which === 13 ){
-            event.preventDefault();
+        else {
+            this.setState({redirect: true});
         }
+        
     }
 
     clear(){
@@ -34,15 +38,18 @@ export default class Navbar extends Component {
     }
 
     render() {
+        if (this.state.redirect) {
+            return <Redirect push to={"/" + this.state.query} />;
+          }
         return (  
             <header>
                 <div className="navbar shadow-sm">
                     <NavLink onClick={this.clear} to='/'><img alt="KinoPortal" src={require("../img/kinoportal-xsm.png")}/></NavLink>
-                    <form onKeyPress={this.handleKeyPress} className="form-inline">
+                    <form onSubmit={this.handleSubmit} className="form-inline">
                     <input ref={input => this.search = input}
          onChange={this._onSelect}  type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2"/>
                     <div className="input-group-append">
-                    <NavLink to={"/"+this.state.query}><button onClick={this.queryInput} className="btn btn-outline-secondary ico-button" type="button" id="button-addon2"><i className="fas fa-search"></i></button></NavLink>
+                    <button onClick={this.handleSubmit} className="btn btn-outline-secondary ico-button" type="button" id="button-addon2"><i className="fas fa-search"></i></button>
                     </div>
                     </form>
                 </div>
